7e143157e7826c50145a0672d13b4222eb2dfb68
Vasanth2308 <vachuvasanth007@gmail.com>
2026-03-25 12:16:00 +0530

    updated the output file

--- aws/env/dev/output.tf
+++ aws/env/dev/output.tf
@@ -2,8 +2,8 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "public_subnet_id" {
-  value = module.vpc.public_subnet_id
+output "subnet_id" {
+  value = module.vpc.subnet_id
 }
 
 output "ec2_instance_id" {
